a061c926008818f9b21fb10a60f1ce2f08adf98c
Nadeesha Dilruwan <nadeesha.dilruwan@otrium.com>
2026-05-23 10:38:28 +0200

    adding site-map and SEO related changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://eclayaz.github.io',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
